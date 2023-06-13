56a91216fdf36f4f2a75bb6b1847f16da30064bd
Shevchenko Andrey <demorest49de@gmail.com>
2023-06-13 22:09:33 +0300

    Changes index.js added;

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-import {el, setChildren} from "redom";
-import axios from "axios";
-import Navigo from "navigo";
+import {el, setChildren} from 'redom';
+import axios from 'axios';
+import Navigo from 'navigo';
 
 const router = new Navigo('/');
 
@@ -8,52 +8,52 @@ const URL = 'https://muddy-substantial-gear.glitch.me/api/';
 
 
 const goodsDetailsPage = (id) => {
-    const body = el(`div`, `Загрузка...`);
-    axios.get(`${URL}/goods/${id}`)
-        .then(({data: {title, description, price}}) => {
-            setChildren(body, [
-                el(`h2`, title),
-                el(`p`, description),
-                el(`p`, `${price} Р`),
-                el(`a`, {
-                    href: '/',
-                    onclick(event) {
-                        event.preventDefault();
-                        router.navigate(event.target.getAttribute('href'));
-                    }
-                }, `вернуться на главную`),
-            ]);
-        });
-    return el(`div`, {className: `container`},
-        [el(`h1`, `Карточка товара`), body]);
+  const body = el(`div`, `Загрузка...`);
+  axios.get(`${URL}/goods/${id}`)
+      .then(({data: {title, description, price}}) => {
+        setChildren(body, [
+          el(`h2`, title),
+          el(`p`, description),
+          el(`p`, `${price} Р`),
+          el(`a`, {
+            href: '/',
+            onclick(event) {
+              event.preventDefault();
+              router.navigate(event.target.getAttribute('href'));
+            },
+          }, `вернуться на главную`),
+        ]);
+      });
+  return el(`div`, {className: `container`},
+      [el(`h1`, `Карточка товара`), body]);
 };
 
 const goodsListPage = () => {
-    const list = el('ul', el('li', "загрузка"));
+  const list = el('ul', el('li', 'загрузка'));
 
-    axios.get(`${URL}goods`)
-        .then(({data}) => {
-            setChildren(list, data.map(({id, title}) =>
-                el('li', el('a', {
-                    href: `/goods/${id}`,
-                    onclick(e) {
-                        e.preventDefault();
-                        router.navigate(event.target.getAttribute('href'));
-                    },
-                }, title))));
-        });
+  axios.get(`${URL}goods`)
+      .then(({data}) => {
+        setChildren(list, data.map(({id, title}) =>
+          el('li', el('a', {
+            href: `/goods/${id}`,
+            onclick(e) {
+              e.preventDefault();
+              router.navigate(event.target.getAttribute('href'));
+            },
+          }, title))));
+      });
 
-    return el('div', {className: 'container'}, [el('h1', 'товары')], list);
+  return el('div', {className: 'container'}, [el('h1', 'товары')], list);
 };
 
 router.on('/', () => {
-    setChildren(document.body, goodsListPage());
+  setChildren(document.body, goodsListPage());
 });
 
 router.on('/goods/:id', (data) => {
-    const {data: {id}} = data;
-    console.log(' : ', data);
-    setChildren(document.body, goodsDetailsPage(id));
+  const {data: {id}} = data;
+  console.log(' : ', data);
+  setChildren(document.body, goodsDetailsPage(id));
 });
 
 router.resolve();
