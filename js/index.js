3297122c951fe7dfef584a327944287cb518ec52
Shevchenko Andrey <demorest49de@gmail.com>
2023-06-14 21:14:35 +0300

    Changes index.html changed; index.js changed;

--- js/index.js
+++ js/index.js
@@ -1,59 +1,146 @@
 import {el, setChildren} from 'redom';
 import axios from 'axios';
-import Navigo from 'navigo';
+import Navigo from "navigo";
 
 const router = new Navigo('/');
 
-const URL = 'https://muddy-substantial-gear.glitch.me/api/';
-
-
-const goodsDetailsPage = (id) => {
-  const body = el(`div`, `Загрузка...`);
-  axios.get(`${URL}/goods/${id}`)
-      .then(({data: {title, description, price}}) => {
-        setChildren(body, [
-          el(`h2`, title),
-          el(`p`, description),
-          el(`p`, `${price} Р`),
-          el(`a`, {
-            href: '/',
-            onclick(event) {
-              event.preventDefault();
-              router.navigate(event.target.getAttribute('href'));
-            },
-          }, `вернуться на главную`),
-        ]);
-      });
-  return el(`div`, {className: `container`},
-      [el(`h1`, `Карточка товара`), body]);
+const form = () => {
+    return el('form', {
+            action: "#",
+            className: "form",
+            id: "form"
+        },
+        [
+            el('div',
+                {
+                    className: 'form__input-wrap form__input-wrap_holder',
+                },
+                [
+                    el('label',
+                        {
+                            htmlFor: "cardName",
+                            className: "form__label form__holder-label",
+                            textContent: 'Card Holder',
+                        }),
+                    el('input',
+                        {
+                            type: 'text',
+                            className: 'input input__holder',
+                            id: 'cardName',
+                        }),
+                ]
+            ),
+            el('div',
+                {
+                    className: 'form__input-wrap form__input-wrap_number',
+                },
+                [
+                    el('label',
+                        {
+                            htmlFor: "cardNumber",
+                            className: "form__label form__holder-label",
+                            textContent: 'Card Number',
+                        }),
+                    el('input',
+                        {
+                            type: 'text',
+                            className: 'input input__number',
+                            id: 'cardNumber',
+                        }),
+                ]
+            ),
+            el('div',
+                {
+                    className: 'form__input-wrap form__input-wrap_date',
+                },
+                [
+                    el('label',
+                        {
+                            htmlFor: "cardDate",
+                            className: "form__label form__holder-label",
+                            textContent: 'Card Expiry',
+                        }),
+                    el('input',
+                        {
+                            type: 'text',
+                            className: 'input input__date',
+                            id: 'cardDate',
+                        }),
+                ]
+            ),
+            el('div',
+                {
+                    className: 'form__input-wrap form__input-wrap_cvv',
+                },
+                [
+                    el('label',
+                        {
+                            htmlFor: "cardCVV",
+                            className: "form__label form__holder-label",
+                            textContent: 'CVV',
+                        }),
+                    el('input',
+                        {
+                            type: 'text',
+                            className: 'input input__cvv',
+                            id: 'cardCVV',
+                        }),
+                ]
+            ),
+            el('button',
+                {
+                    className: 'form__button',
+                    textContent: 'CHECK OUT',
+                })
+        ]
+    );
 };
 
-const goodsListPage = () => {
-  const list = el('ul', el('li', 'загрузка'));
 
-  axios.get(`${URL}goods`)
-      .then(({data}) => {
-        setChildren(list, data.map(({id, title}) =>
-          el('li', el('a', {
-            href: `/goods/${id}`,
-            onclick(e) {
-              e.preventDefault();
-              router.navigate(event.target.getAttribute('href'));
-            },
-          }, title))));
-      });
+const creditCard = () => {
 
-  return el('div', {className: 'container'}, [el('h1', 'товары')], list);
+    return el('div', {
+            className: 'credit-card',
+        },
+        [
+            el('span',
+                {
+                    className: 'card__number',
+                    textContent: 'xxxx xxxx xxxx xxxx'
+                }),
+            el('div',
+                {
+                    className: 'card__personal',
+                },
+                [
+                    el('span', {className: "card__name", textContent: 'John Doe'}),
+                    el('span', {className: "card__date", textContent: '04/24'}),
+                ]
+            ),
+        ]
+    );
+};
+
+const card = () => {
+    const secure = el('p', {
+            className: 'secure',
+            textContent: 'Secure Checkout',
+        },
+    );
+
+    return el('div', {
+            className: 'card'
+        }, [secure, creditCard(), form()]
+    );
+};
+
+const wrapper = () => {
+
+    return el('div', {className: 'wrapper'}, card());
 };
 
 router.on('/', () => {
-  setChildren(document.body, goodsListPage());
-});
-
-router.on('/goods/:id', (data) => {
-  const {data: {id}} = data;
-  console.log(' : ', data);
-  setChildren(document.body, goodsDetailsPage(id));
+    setChildren(document.body, wrapper());
 });
 
 router.resolve();
